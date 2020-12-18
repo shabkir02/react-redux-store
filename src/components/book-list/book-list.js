d1e223aa2824e0a40646a2535d33ec372bca3e95
Kirill <shabkir02@gmail.com>
2020-12-18 22:42:34 +0600

    setting

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -5,22 +5,25 @@ import { connect } from 'react-redux';
 import { withBookstoreService } from '../hoc';
 import { booksLoaded } from '../../actions';
 import { compose } from '../../utils';
+import Spinner from '../spinner';
 
 import './book-list.css';
 
 class BookList extends Component {
 
   componentDidMount() {
-    // 1. receive data
-    const { bookstoreService } = this.props;
-    const data = bookstoreService.getBooks();
-
-    // 2. dispacth action to store
-    this.props.booksLoaded(data);
+    const { bookstoreService, booksLoaded } = this.props;
+    bookstoreService.getBooks()
+      .then((data) => {
+        booksLoaded(data)
+      });
   }
 
   render() {
-    const { books } = this.props;
+    const { books, loading } = this.props;
+    if (loading) {
+      return <Spinner/>
+    }
     return (
       <ul className="book-list">
         {
@@ -35,8 +38,8 @@ class BookList extends Component {
   }
 }
 
-const mapStateToProps = ({ books }) => {
-  return { books };
+const mapStateToProps = ({ books, loading }) => {
+  return { books, loading };
 };
 
 const mapDispatchToProps = {
