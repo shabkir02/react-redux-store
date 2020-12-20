77e7693cfd5c30e687b83c746f17fc274db84e45
Kirill <shabkir02@gmail.com>
2020-12-20 22:27:44 +0600

    bookList shopingCart

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { BookstoreServiceProvider } from './components/bookstore-service-context
 import store from './store';
 
 const bookstoreService = new BookstoreService();
+console.dir(bookstoreService);
 
 ReactDOM.render(
   <Provider store={store}>
